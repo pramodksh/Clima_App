75e1b4e4d97a4011d3cac504038e5c1b115716d2
pramodksh <pramodkumarsh3@gmail.com>
2022-07-07 14:09:01 +0530

    Integrated OpenWeatherAPI and displaying weather description

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(MyApp());
+Future<void> main()async{
+  await dotenv.load(
+    fileName: '.env',
+  );
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
 
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,26 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
+
   @override
   State<LoadingScreen> createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double? longitude;
+  double? latitude;
+  var openWeatherAPIKeyVar = dotenv.env['openWeatherAPIKey'];
+
+  void getWeather() async {
+    var OpenWeatherAPIUrl = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openWeatherAPIKeyVar}');
+
+    Response response = await get(OpenWeatherAPIUrl);
+
+    // print(response.body);
+    var data = jsonDecode(response.body);
+    var weatherDescription =  data['weather'][0]['description'];
+    print(weatherDescription);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: ()  async{
+          onPressed: () async {
             LocationService myLocation = LocationService();
             await myLocation.getCurrentLocation();
-            print(myLocation.getLong());
-            print(myLocation.getLat());
-
+            longitude = await myLocation.getLong();
+            latitude = await myLocation.getLat();
+            getWeather();
           },
           child: Text('Get Location'),
         ),
